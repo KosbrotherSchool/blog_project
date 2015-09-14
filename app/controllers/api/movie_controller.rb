18aa920ebf3b3db11bed6269558434a33c06599a
Ko LiChung <kerlichung@gmail.com>
2015-09-14 16:33:26 +0800

    fix movie api

--- app/controllers/api/movie_controller.rb
+++ app/controllers/api/movie_controller.rb
@@ -28,7 +28,11 @@ class Api::MovieController < ApplicationController
     elsif params[:movie_round] != nil
       if params[:movie_round].to_i != 4
           movie_round = params[:movie_round].to_i
-          movies = Movie.where("movie_round = #{movie_round} and yahoo_link is not NULL and is_this_week_new = false").order('publish_date_date DESC').paginate(:page => params[:page], :per_page => 10)
+          if movie_round != 3 
+            movies = Movie.where("movie_round = #{movie_round} and yahoo_link is not NULL and is_this_week_new = false").order('publish_date_date DESC').paginate(:page => params[:page], :per_page => 10)
+          else
+            movies = Movie.where("movie_round = #{movie_round} and yahoo_link is not NULL and is_this_week_new = false").order('publish_date_date ASC').paginate(:page => params[:page], :per_page => 10)
+          end
           render :json => movies
       else
           movies = Movie.where("is_this_week_new = true and yahoo_link is not NULL")
