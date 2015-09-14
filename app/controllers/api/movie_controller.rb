764814bebf12d3487f9c1dc29f16a87dc49fddfa
Ko LiChung <kerlichung@gmail.com>
2015-09-15 07:01:28 +0800

    improve search api

--- app/controllers/api/movie_controller.rb
+++ app/controllers/api/movie_controller.rb
@@ -2,7 +2,7 @@ class Api::MovieController < ApplicationController
 
   def search
     query = params[:query]
-    movies = Movie.where('title LIKE ? OR title_eng LIKE ? OR actors LIKE ?', "%#{query}%", "%#{query}%", "%#{query}%").paginate(:page => params[:page], :per_page => 10)
+    movies = Movie.select('id, title, publish_date, movie_class, actors, movie_type, small_pic').where('title LIKE ? OR title_eng LIKE ? OR actors LIKE ?', "%#{query}%", "%#{query}%", "%#{query}%").paginate(:page => params[:page], :per_page => 10)
     render :json => movies 
   end
 
