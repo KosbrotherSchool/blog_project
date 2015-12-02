199c153fddbb032606b32d33f006f8189a91f5d5
Ko LiChung <kerlichung@gmail.com>
2015-12-02 15:03:55 +0800

    update upgoing api

--- app/controllers/api2/movie_controller.rb
+++ app/controllers/api2/movie_controller.rb
@@ -42,9 +42,9 @@ class Api2::MovieController < ApplicationController
 
   def up_going_movies
     if params[:page] != nil
-      movies = Movie.select('id, title, small_pic, large_pic, point, review_size').where("movie_round = 3 and yahoo_link is not NULL and is_this_week_new = false").order('publish_date_date ASC').paginate(:page => params[:page], :per_page => 20)
+      movies = Movie.select('id, title, small_pic, large_pic, publish_date').where("movie_round = 3 and yahoo_link is not NULL and is_this_week_new = false").order('publish_date_date ASC').paginate(:page => params[:page], :per_page => 20)
     else
-      movies = Movie.select('movies.id, title, small_pic, large_pic, point, review_size').where("movie_round = 3").order('publish_date_date ASC')
+      movies = Movie.select('id, title, small_pic, large_pic, publish_date').where("movie_round = 3").order('publish_date_date ASC')
     end
     render :json => movies
   end
